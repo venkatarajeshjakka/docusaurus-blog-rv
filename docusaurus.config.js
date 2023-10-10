25df3bec930f005b828d5f8975a6b88ee49814c2
Venkata Rajesh Jakka <rajesh.jakka1@gmail.com>
2023-10-10 12:30:13 +0530

    Changed the name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'too-slow',
+        title: 'Infinity',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
